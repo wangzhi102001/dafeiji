0292bcd59b6f19425d7bdbc49704ae3dada2a430
wangzhi102001 <wangzhi102001@live.com>
2018-03-18 17:49:39 +0800

    20180318

--- dafeiji/dafeiji.py
+++ dafeiji/dafeiji.py
@@ -3,21 +3,23 @@ import pygame
 from setting import Setting
 from ship import Ship
 import game_functions as gf
+from pygame.sprite import Group
 
 
 def run_game():
     pygame.init()
     ai_setting = Setting()  # 导入设置
-    screen = pygame.display.set_mode(
-        (ai_setting.screen_wide, ai_setting.screen_height))
+    screen = pygame.display.set_mode((ai_setting.screen_wide, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invasion")  # 设置屏幕宽度、高度，标题
 
     ship = Ship(ai_setting, screen)
-
+    
+    bullets = Group()
     while True:
-        gf.check_event(ship)
+        gf.check_event(ai_setting,screen,ship,bullets)
         ship.update()
-        gf.update_screen(ai_setting, screen, ship)
+        gf.update_bullets(bullets)
+        gf.update_screen(ai_setting, screen, ship,bullets)
 
 
 run_game()
--- dafeiji/game_functions.py
+++ dafeiji/game_functions.py
@@ -2,16 +2,17 @@
 # -*- coding:utf-8 -*-
 import pygame
 import sys
+from bullet import Bullet
 
 
 # from ship import Ship
-def check_event(ship):
+def check_event(ai_setting,screen, ship,bullets):
     # 响应鼠标和键盘事件
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_key_down_events(event, ship)
+            check_key_down_events(event, ai_setting,screen, ship,bullets)
         elif event.type == pygame.KEYUP:
             check_key_up_events(event, ship)
 
@@ -24,16 +25,35 @@ def check_key_up_events(event, ship):
         ship.move_left = False
 
 
-def check_key_down_events(event, ship):
+def check_key_down_events(event, ai_setting,screen, ship,bullets):
     # 响应“按下按键”事件
     if event.key == pygame.K_RIGHT:
         ship.move_right = True
     elif event.key == pygame.K_LEFT:
         ship.move_left = True
+    elif event.key == pygame.K_SPACE:
+        fire_bullet(ai_setting,screen,ship,bullets)
 
 
-def update_screen(ai_setting, screen, ship):
+def update_screen(ai_setting, screen, ship,bullets):
     # 更新屏幕数据
     screen.fill(ai_setting.bg_color)
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
+
     ship.bliteme()
     pygame.display.flip()
+
+def update_bullets(bullets):
+    bullets.update()
+    for bullet in bullets.copy():
+        if bullet.rect.bottom<=0:
+            bullets.remove(bullet)
+            #print(len(bullets))
+
+def fire_bullet(ai_setting,screen,ship,bullets):
+    if len(bullets)<ai_setting.bullet_allowed:
+            new_bullet = Bullet(ai_setting,screen,ship)
+            bullets.add(new_bullet)
+
+
