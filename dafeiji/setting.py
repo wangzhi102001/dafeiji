d7a62a327e751f470a64cd487dfae6e1f4158f18
wangzhi102001 <35896226+wangzhi102001@users.noreply.github.com>
2018-04-08 19:14:18 +0800

    001

--- dafeiji/setting.py
+++ dafeiji/setting.py
@@ -6,10 +6,10 @@ class Setting():
         self.screen_wide = 1200
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
-        self.ship_speed_factor = 3
-        self.bullet_speed_factor = 5  #子弹射速
-        self.bullet_width = 10   #子弹宽度
-        self.bullet_height = 40  #子弹长度
+        self.ship_speed_factor = 2
+        self.bullet_speed_factor = 2  #子弹射速
+        self.bullet_width = 5   #子弹宽度
+        self.bullet_height = 30  #子弹长度
         self.bullet_color = 60,60,60  #子弹颜色
-        self.bullet_allowed = 8   #最大子弹允许数量
+        self.bullet_allowed = 5   #最大子弹允许数量
 
